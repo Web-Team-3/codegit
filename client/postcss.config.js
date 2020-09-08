cc58331d61f6f413a31c4488455f25f6630459bc
Ankit Chakraborty <ankitchakraborty700@gmail.com>
2020-09-08 18:17:49 +0530

    landing page update

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,8 +1,66 @@
+
 const tailwindcss = require('tailwindcss');
+module.exports = {
+    purge: [],
+    theme: {
+      extend: {
+        colors: {
+          blue: {
+              1000:'#366D93',
+          },
+          indigo: {
+              1000:'#1D2D50',
+          },
+          purple: {
+              1000:'#133B5C',
+          },
+          gray: {
+              1000:'#06151F',
+          },
+        },
+      },
+    },
+    variants: {},
+    plugins: [
+      tailwindcss('./tailwind.js'),
+      require('autoprefixer'),
+    ],
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const tailwindcss = require('tailwindcss');
 
 module.exports = {
+    extend: {
+        colors: {
+          blue: {
+              1000:'#366D93',
+          },
+          indigo: {
+              1000:'#1D2D50',
+          },
+          purple: {
+              1000:'#133B5C',
+          },
+          gray: {
+              1000:'#06151F',
+          },
+        },
+      },
     plugins: [
         tailwindcss('./tailwind.js'),
         require('autoprefixer')
-    ]
-}
+    ],
+}*/
